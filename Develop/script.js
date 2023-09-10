28ab6fea19e7e21f44a53cc6005773eec6bc9b8d
stasbaz <bazuluk989@gmail.com>
2023-09-09 22:08:59 -0600

    Final version of project

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,79 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+//More variables for future password
+var characters = 'abcdef';
+var caps = 'GHIJKL';
+var numbers = '213895';
+var symbols = '%$@-&#';
+var includeChars;
+var includeCaps;
+var includeNumbers;
+var includeSymbols;
+
+var mix;
+
+
+var passwordLength = prompt('How many characters you want?');
+
+if (isNaN(passwordLength)) {
+  alert('You must enter a valid number.');
+
+  var passwordLength = prompt('How many characters you want?');
+}
+
+if(passwordLength < 8) {
+  alert('Password should be 8 minimum characters');
+
+  var passwordLength = prompt('How many characters you want?');
+}
+
+if(passwordLength > 128) {
+  alert('Password should be 128 characters maximum');
+
+  var passwordLength = prompt('How many characters you want?');
+}
+
+includeChars = confirm('Include characters?');
+includeCaps =  confirm('Include Uppercase letters?');
+includeNumbers = confirm('Include numbers?');
+includeSymbols = confirm('Include symbols?');
+
+var blend = '';
+
+if(includeChars) {
+  blend += characters;
+}
+if(includeCaps) {
+  blend += caps;
+}
+if(includeNumbers){
+  blend += numbers;
+}
+if(includeSymbols){
+  blend += symbols;
+}
+
+mix = includeChars + includeCaps + includeNumbers + includeSymbols;
+
+function generatePassword() {
+  var password = '';
+
+  for (var index = 0; index < passwordLength; index++) {
+    var randomIndex = Math.floor(Math.random() * blend.length);
+    password += blend.charAt(randomIndex);
+  }
+
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Password Generator Starter Code
+# Password Generator
+
+This is a simple password generator, that can generate passwords based on users' criteria.
+
+## Features 
+
+- Customizable password between 8 and 128 characters.
+- Options to include lowercase, uppercase, numbers, and symbols
+- You must write exactly how long your password should be to generate it, otherwise, due to incorrectly entered data, the generator will ask you again.
+
+## Usage
+
+This password generator can create random strong password for, based on your whims.
+
+## How to Use
+
+- Open the password generator application in your web browser.
+- Click the "Generate Password" button.
+- You will be prompted to specify your password criteria:
+Enter the desired password length (between 8 and 128 characters).
+Confirm whether you want to include lowercase letters, uppercase letters, numbers, and special symbols.
+- Click "OK" or "Cancel" on the prompts to select or deselect the criteria.
+- After confirming your criteria, the generated password will be displayed in the designated area on the page.
+- You can now copy the password and use it for your accounts.
+
+## Screenshot Of My Generator
+![Screenshot of my working generator](../friendly-parakeet/images/generator.png)
